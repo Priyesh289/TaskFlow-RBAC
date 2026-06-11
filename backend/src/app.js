c352a0870f3da987c9d44c3fafcc1ece2143221c
priyesh shukla <priyesh1820@gmail.com>
2026-06-11 18:39:23 +0530

    inital backend setup

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Task Management API Running",
+  });
+});
+
+export default app;
